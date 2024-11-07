121d2229d5d6fa0622bdb713ed9e2f929eec9bea
Mari <Mari@DESKTOP-SOBP2O8>
2024-11-07 16:27:06 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Program.cs
@@ -0,0 +1,34 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Obter a string de conexão
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configurar o DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

a251d66564533e2ae0b969202c2590791b276955
GustavoHBarth <barthgustavo5@gmail.com>
2024-12-07 16:52:07 -0300

    Validação Personalizada

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,17 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage = "O nome é ogrigatório")]
     [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
+    //[PrimeiraLetraMaiscula] 
     public string? Nome { get; set; }
 
     [Required]
@@ -35,5 +37,20 @@ public class Produto
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new
+                    ValidationResult("A primeira letra do produto deve ser maiúscula",
+                    new[]
+                    { nameof(this.Nome)}
+                    );
+            }
+        }
+    }
 }
 

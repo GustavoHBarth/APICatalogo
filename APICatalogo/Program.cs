af7831bc77f2eee22b13acc9d2b08fae40982dc9
GustavoHBarth <barthgustavo5@gmail.com>
2024-12-18 15:08:29 -0300

    Aplicação do Uni Of Work

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,42 +1,40 @@
 using APICatalogo.Context;
-using Microsoft.EntityFrameworkCore;
-using APICatalogo.Services;
-using System.Text.Json.Serialization;
-using APICatalogo.Extensions;
 using APICatalogo.Filters;
+using APICatalogo.Logging;
 using APICatalogo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add(typeof(ApiExceptionFilter));
 })
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    });
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Obter a string de conexão
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Configurar o DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
-
-builder.Services.AddTransient<IMeuServico, MeuServico>();
+                    options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddScoped<ApiLoggingFilter>();
 
 builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
 
 var app = builder.Build();
 
@@ -45,13 +43,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- APICatalogo/Repositories/Repository.cs
+++ APICatalogo/Repositories/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace APICatalogo.Repositories;
@@ -11,31 +12,34 @@ public class Repository<T> : IRepository<T> where T : class
     {
         _context = context;
     }
+
     public IEnumerable<T> GetAll()
     {
-        return _context.Set<T>().ToList();
+        return _context.Set<T>().AsNoTracking().ToList();
     }
+
     public T? Get(Expression<Func<T, bool>> predicate)
     {
         return _context.Set<T>().FirstOrDefault(predicate);
     }
+
     public T Create(T entity)
     {
         _context.Set<T>().Add(entity);
-        _context.SaveChanges();
+        //_context.SaveChanges();
         return entity;
     }
     public T Update(T entity)
     {
         _context.Set<T>().Update(entity);
-        _context.SaveChanges ();
+        //_context.Entry(entity).State = EntityState.Modified;
+        //_context.SaveChanges();
         return entity;
     }
     public T Delete(T entity)
     {
         _context.Set<T>().Remove(entity);
-        _context.SaveChanges ();
+        //_context.SaveChanges();
         return entity;
     }
- 
 }

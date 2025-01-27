4861918bfe4969a7af0792c5456dcbcefa42ed36
GustavoHBarth <barthgustavo5@gmail.com>
2025-01-27 20:03:01 -0300

    AuthController - Revoke

--- APICatalogo/Controllers/AuthController.cs
+++ APICatalogo/Controllers/AuthController.cs
@@ -159,5 +159,19 @@ public class AuthController : ControllerBase
         });
     }
 
-    
+    [Authorize]
+    [HttpPost]
+    [Route("revoke/{username}")]
+    public async Task<IActionResult> Revoke(string username)
+    {
+        var user = await _userManager.FindByNameAsync(username);
+
+        if (user == null) return BadRequest("Invalid user name");
+
+        user.RefreshToken = null;
+
+        await _userManager.UpdateAsync(user);
+
+        return NoContent();
+    }
 }
